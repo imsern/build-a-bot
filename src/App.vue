c576e25e8ec57cf9470bb2380af475f3f61fb2ee
imsern <kristofferims@hotmail.com>
2022-03-28 09:57:05 +0200

    init push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+            <img src="./assets/build-a-bot-logo.png" alt="logo">
+            Build-a-Bot
+        </li>
+        <li>
+          User: {{ user.userName }}
+          <button @click="changeUserName()">Change</button>
+        </li>
+      </ul>
+    </nav>
+  </header> -->
+  <main>
+    <RobotBuilder />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
+// import Search from './search/Search.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RobotBuilder,
+    // Search,
   },
+  // data() {
+  //   return {
+  //     user: { userName: 'Kristoffer' },
+  //   };
+  // },
+  provide() {
+    return { user: this.user };
+  },
+  // methods: {
+  //   changeUserName() {
+  //     this.user.userName = 'Stigh';
+  //   },
+  // },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+.nav-item {
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
 }
 </style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div>
+    <img src="../assets/robot-home.png" alt="robot" aria-hidden="true" class="robot">
+    </div>
+    <div class="get-started">
+      <a href="">Get Started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
